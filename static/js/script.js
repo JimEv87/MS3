2c4e7307ac98c7262143e8384749f4e04a614c08
JimEv87 <jimevans212@gmail.com>
2021-01-11 14:13:41 +0000

    made slideshow responsive

--- static/js/script.js
+++ static/js/script.js
@@ -2,8 +2,31 @@ $(document).ready(function () {
     $(".sidenav").sidenav({ edge: "right" });
     $('.slideshow').slick({
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        speed: 3000,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            }
+        ]
+
     });
 });
 
