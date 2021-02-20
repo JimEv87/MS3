b8586ac57cd31bbec7dcca958907ae1bd3dd9fc3
JimEv87 <jimevans212@gmail.com>
2021-02-20 15:57:42 +0000

    some defensive programming added to prevent double spaces

--- static/js/script.js
+++ static/js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
         infinite: true,
         dots: false,
         arrows: true,
-        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
         speed: 300,
         autoplaySpeed: 60000,
         autoplay: true,
@@ -13,7 +13,7 @@ $(document).ready(function () {
         slidesToScroll: 1,
         mobileFirst: true,
         responsive: [
-            
+
             {
                 breakpoint: 500,
                 settings: {
@@ -38,35 +38,60 @@ $(document).ready(function () {
         ]
 
     });
+
+
 });
 
-var input = document.getElementById('username');
+// var input = document.getElementById('username');
 
-input.oninvalid = function(event) {
-    event.target.setCustomValidity('Username should be between 5 and 15 characters: letters and numbers only');
-}
+// input.oninvalid = function (event) {
+//     event.target.setCustomValidity('Username should be between 5 and 15 characters: letters and numbers only');
+// }
 
-var input = document.getElementById('password');
+// var input = document.getElementById('password');
 
-input.oninvalid = function(event) {
-    event.target.setCustomValidity('Password should be between 5 and 15 characters: letters and numbers only');
-}
+// input.oninvalid = function (event) {
+//     event.target.setCustomValidity('Password should be between 5 and 15 characters: letters and numbers only');
+// }
 
 
 // to ensure only numbers are inserted for recipe times
 function validate(evt) {
-  var theEvent = evt || window.event;
+    var theEvent = evt || window.event;
 
-  if (theEvent.type === 'paste') {
-      key = event.clipboardData.getData('text/plain');
-  } else {
-      var key = theEvent.keyCode || theEvent.which;
-      key = String.fromCharCode(key);
-  }
-  var regex = /[0-9]|\./;
-  if( !regex.test(key) ) {
-    theEvent.returnValue = false;
-    if(theEvent.preventDefault) theEvent.preventDefault();
-  }
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if (!regex.test(key)) {
+        theEvent.returnValue = false;
+        if (theEvent.preventDefault) theEvent.preventDefault();
+    }
 }
 
+
+// replace any double-spaces with single-spaces
+let dishName = $("textarea#dish_name");
+$(dishName).keyup(function() {
+    let dishNameValue = this.value;
+    let removeDoubleSpaces = dishNameValue.replace("  ", " ");
+    $(dishName).val(removeDoubleSpaces);
+});
+
+
+let ingredients = $("textarea#ingredients");
+$(ingredients).keyup(function() {
+    let ingredientsValue = this.value;
+    let removeDoubleSpaces = ingredientsValue.replace("  ", " ");
+    $(ingredients).val(removeDoubleSpaces);
+});
+
+let method = $("textarea#method");
+$(method).keyup(function() {
+    let methodValue = this.value;
+    let removeDoubleSpaces = methodValue.replace("  ", " ");
+    $(method).val(removeDoubleSpaces);
+});
