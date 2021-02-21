24ec4b7e197e76ee2ea6f284181e1c6064833751
JimEv87 <jimevans212@gmail.com>
2021-02-21 15:52:59 +0000

    slide settings corrected

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+//slide settings
 $(document).ready(function () {
     $(".sidenav").sidenav({ edge: "right" });
     $('.slideshow').slick({
@@ -7,7 +8,7 @@ $(document).ready(function () {
         prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
         nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
         speed: 300,
-        autoplaySpeed: 60000,
+        autoplaySpeed: 5000,
         autoplay: true,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -19,6 +20,7 @@ $(document).ready(function () {
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
+                    autoplaySpeed: 6000,
                 }
             },
             {
@@ -26,6 +28,7 @@ $(document).ready(function () {
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
+                    autoplaySpeed: 7000,
                 }
             },
             {
@@ -33,6 +36,7 @@ $(document).ready(function () {
                 settings: {
                     slidesToShow: 4,
                     slidesToScroll: 4,
+                    autoplaySpeed: 9000,
                 }
             },
         ]
@@ -41,19 +45,6 @@ $(document).ready(function () {
 
 
 });
-
-// var input = document.getElementById('username');
-
-// input.oninvalid = function (event) {
-//     event.target.setCustomValidity('Username should be between 5 and 15 characters: letters and numbers only');
-// }
-
-// var input = document.getElementById('password');
-
-// input.oninvalid = function (event) {
-//     event.target.setCustomValidity('Password should be between 5 and 15 characters: letters and numbers only');
-// }
-
 
 // to ensure only numbers are inserted for recipe times
 function validate(evt) {
@@ -75,7 +66,7 @@ function validate(evt) {
 
 // replace any double-spaces with single-spaces
 let dishName = $("textarea#dish_name");
-$(dishName).keyup(function() {
+$(dishName).keyup(function () {
     let dishNameValue = this.value;
     let removeDoubleSpaces = dishNameValue.replace("  ", " ");
     $(dishName).val(removeDoubleSpaces);
@@ -83,19 +74,21 @@ $(dishName).keyup(function() {
 
 
 let ingredients = $("textarea#ingredients");
-$(ingredients).keyup(function() {
+$(ingredients).keyup(function () {
     let ingredientsValue = this.value;
     let removeDoubleSpaces = ingredientsValue.replace("  ", " ");
     $(ingredients).val(removeDoubleSpaces);
 });
 
 let method = $("textarea#method");
-$(method).keyup(function() {
+$(method).keyup(function () {
     let methodValue = this.value;
     let removeDoubleSpaces = methodValue.replace("  ", " ");
     $(method).val(removeDoubleSpaces);
 });
 
+
+// triggers 'loadup' animation shrink
 window.onload = function () {
-            document.documentElement.classList.remove('loadup');
-        };
+    document.documentElement.classList.remove('loadup');
+};
